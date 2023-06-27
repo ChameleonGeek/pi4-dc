5929e6e9465f9f80c96d2322b781f2e7687f55e6
ChameleonGeek <55959244+ChameleonGeek@users.noreply.github.com>
2023-06-26 20:32:30 -0700

    Update minerva2.sh

--- minerva2.sh
+++ minerva2.sh
@@ -21,38 +21,46 @@ EchoColor(){ # color, text
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${RED}" "Answer No to VALIDATE PASSWORD COMPONENT!"
 EchoColor "${YEL}" "phpMyAdmin install will fail if you answer Yes"
+EchoColor "${GRN}" "####################################################################################################"
 
 # shore up mySQL
 sudo mysql_secure_installation
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing webmin prerequisites"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install apt-transport-https -y
 sudo apt install software-properties-common -y
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing webmin"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install webmin -y
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Allowing webmin through the firewall"
+EchoColor "${GRN}" "####################################################################################################"
 sudo ufw allow 10000
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing phpmyadmin"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install phpmyadmin -y 
 sudo apt install php-mbstring -y 
 sudo phpenmod mbstring
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing samba and support for exFAT drives"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install samba -y
 sudo apt install exfat-fuse -y
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing FTP server"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install proftpd -y
 
 EchoColor "${GRN}" "####################################################################################################"
 EchoColor "${GRN}" "Installing bind DNS Server"
+EchoColor "${GRN}" "####################################################################################################"
 sudo apt install bind9 bind9utils bind9-doc dnsutils -y
